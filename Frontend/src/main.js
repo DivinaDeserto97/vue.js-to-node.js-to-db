167501d271611abbec5d8b1d72ce5897ef76c64e
Divina Deserto <117446023+DivinaDeserto97@users.noreply.github.com>
2023-09-06 15:46:21 +0200

    Bootsrtap instaliert und eingebunden

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,5 +1,9 @@
 import './assets/main.css'
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
