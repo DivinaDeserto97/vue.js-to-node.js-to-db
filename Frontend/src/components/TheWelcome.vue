1cea33bbe12fb14e20cbc98286b3d36adae8f5e9
Divina Deserto <117446023+DivinaDeserto97@users.noreply.github.com>
2023-09-06 22:26:36 +0200

    Font Aesome instaliert

--- Frontend/src/components/TheWelcome.vue
+++ Frontend/src/components/TheWelcome.vue
@@ -3,7 +3,7 @@
 <template>
   <dev class="row">Hallo User</dev>
   <dev class="row">
-    <button type="button" class="btn btn-primary col">Login</button>
-    <button type="button" class="btn btn-success col">Registrieren</button>
+    <button type="button" class="btn btn-primary col"><i class="fa-solid fa-arrow-right"></i><i class="fa-solid fa-user"></i></button>
+    <button type="button" class="btn btn-success col"><i class="fa-solid fa-user-plus"></i></button>
   </dev>
 </template>
