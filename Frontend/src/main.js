bca3f7f3a65f330872dccfebf7d31a7193a76961
Divina Deserto <117446023+DivinaDeserto97@users.noreply.github.com>
2023-09-09 00:41:49 +0200

    main.js komentiert und ctrl+ shift+ i

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,8 +1,8 @@
-import '@fortawesome/fontawesome-pro/scss/fontawesome.scss';
-import '@fortawesome/fontawesome-pro/scss/regular.scss';  // Für reguläre Icons
-import '@fortawesome/fontawesome-pro/scss/solid.scss';    // Für solide Icons
-import '@fortawesome/fontawesome-pro/scss/brands.scss';   // Für Markenicons
-import '@fortawesome/fontawesome-pro/scss/light.scss';    // Für leichte Icons
+import "@fortawesome/fontawesome-pro/scss/fontawesome.scss";
+import "@fortawesome/fontawesome-pro/scss/regular.scss"; // Für reguläre Icons
+import "@fortawesome/fontawesome-pro/scss/solid.scss"; // Für solide Icons
+import "@fortawesome/fontawesome-pro/scss/brands.scss"; // Für Markenicons
+import "@fortawesome/fontawesome-pro/scss/light.scss"; // Für leichte Icons
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
